0682b3ccd7539fa00aefabc81737ffdb4a82e796
WilsonLimSet <limsetia@usc.edu>
2023-03-12 22:38:30 -0700

    version 0.1 done

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import '@pages/options/Options.css';
 
 export default function Options(): JSX.Element {
-  return <div className="container">Options HAHAH</div>;
+  return <div className="container">Options to come soon</div>;
 }
